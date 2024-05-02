0c095fd3b3b58d9975f0fc1d3bc90cd5c5f3bc6d
Mohammed Mansoor P K <pkmansoor@proton.me>
2024-05-02 12:52:32 +0530

    html,css,animation are modified

--- views/animation/animation.js
+++ views/animation/animation.js
@@ -1,45 +1,16 @@
-var input = document.querySelector("input");
-var para = document.querySelector("p");
-var text = para.innerHTML;
-var words = text.split(" ");
-var ids = [];
-var index = 0;
-function highlightSearch() {
-  for (i = 0; i <= words.length; i++) {
-    if (words[i] === input.value) {
-      words[i] = "<span class='colors' id=" + i + ">" + input.value + "</span>";
-      ids.push(i);
-      para.innerHTML = words.join(" ");
-    }
-  }
-  document.getElementById("result").innerHTML = `Search result : ${
-    ids.length
-  } `;
-}
-function next() {
-  if (index != 0 || index > 0 || index < 0) {
-    document.getElementById(ids[index - 1]).style.color = "white";
-    document.getElementById(ids[index - 1]).style.background = "red";
-  }
-  document.getElementById(ids[index]).style.color = "white";
-  document.getElementById(ids[index]).style.background = "orange";
-  document.getElementById(ids[index]).scrollIntoView();
-  if (ids.length - 1 == index) {
-    index = 0;
-  } else {
-    index++;
-  }
-}
-function prev() {
-  if (index == 1) {
-    return false;
-  }
-  if (ids[index - 1] == ids[index - 1]) {
-    document.getElementById(ids[index - 1]).style.color = "white";
-    document.getElementById(ids[index - 1]).style.background = "red";
-    index--;
-  }
-  document.getElementById(ids[index]).scrollIntoView();
-  document.getElementById(ids[index - 1]).style.color = "white";
-  document.getElementById(ids[index - 1]).style.background = "orange";
-}
+const section = document.querySelector(".hide-section");
+            const formContainer = document.querySelector(".login-container");
+            const loginContainer = document.querySelector(".login");
+            const profile = document.querySelector("img");
+
+
+            profile.onclick = () => {
+               requestAnimationFrame(() => {
+                  loginContainer.classList.remove("hide-login");
+                  formContainer.classList.remove("hide-form");
+
+                  setTimeout(() => {
+                     section.classList.remove("hide-section");
+                  }, 1000);
+               });
+            };
